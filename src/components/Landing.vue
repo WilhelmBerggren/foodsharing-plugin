dd3bcaafbe9ce786ff7bcbf554c257ea96580a14
Wilhelm Berggren <wilhelm.berggren@humly.io>
2026-03-11 11:31:27 +0100

    Fix foodsaving -> foodsharing

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="column items-center justify-center q-pa-xl">
-    <img :src="logo" alt="FoodSaving logo" style="max-width: 100%; width: 600px;" />
+    <img :src="logo" alt="FoodSharing logo" style="max-width: 100%; width: 600px;" />
     <div class="row q-gutter-md q-mt-md">
       <QBtn unelevated color="primary" label="Log in" to="/login" />
       <QBtn unelevated outline color="primary" label="Sign up" to="/signup" />
